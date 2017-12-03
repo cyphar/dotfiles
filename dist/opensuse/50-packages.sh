c98d5ae5c459bdf5c3213cf5a8e8ae193c953d71
Aleksa Sarai <cyphar@cyphar.com>
2017-12-03 17:39:21 +1100

    opensuse: add {skopeo,umoci,runc}
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- dist/opensuse/50-packages.sh
+++ dist/opensuse/50-packages.sh
@@ -39,6 +39,8 @@ packages=(
 	"neovim" "tmux" "zsh" "git" "gcc" "go" "keychain" "figlet" "gpg2" "python3"
 	"mosh" "rsync" "ranger" "alsa-utils" "weechat" "make" "exfat-utils"
 	"fuse-exfat" "xfsprogs" "autoconf" "automake" "libtool"
+	# Container-related packages.
+	"skopeo" "umoci" "runc"
 	# Basic graphics stack and environment.
 	"i3" "i3lock" "i3status" "dmenu" "ImageMagick" "xorg-x11-server"
 	"xf86-video-intel" "xf86-input-keyboard" "xf86-input-mouse" "compton"
