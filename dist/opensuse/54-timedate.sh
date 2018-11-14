d52bd37959e40460ba5ef09392a1f8e1932cf9e1
Aleksa Sarai <cyphar@cyphar.com>
2018-11-15 01:41:04 +1100

    dist: opensuse: enable ntp
    Why on earth isn't this the default. Such insanity.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

---
+++ dist/opensuse/54-timedate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# dotfiles: collection of my personal dotfiles [code]
+# Copyright (C) 2018 Aleksa Sarai <cyphar@cyphar.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+echo ">> configure timezone and ntpd"
+timedatectl set-timezone 'Australia/Sydney'
+timedatectl set-local-rtc false
+timedatectl set-ntp true
